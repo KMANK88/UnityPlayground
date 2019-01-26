03c02671f1a39af8379707615761792eeed6f721
KMANK88 <karstenpease88@outlook.com>
2019-01-26 11:07:31 -0700

    first commit of game

---
+++ Assets/alienMovement.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class alienMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start(){
+        int rand = Random.Range(0, 4);
+        if(rand == 0)
+        {
+            this.GetComponent<Rigidbody2D>().velocity = new Vector2(0f, -5f);
+        }else if (rand == 1)
+        {
+            this.GetComponent<Rigidbody2D>().velocity = new Vector2(0f, 5f);
+
+        }
+        else if (rand == 2)
+        {
+            this.GetComponent<Rigidbody2D>().velocity = new Vector2(5f, 0f);
+
+        }
+        else if (rand == 3)
+        {
+            this.GetComponent<Rigidbody2D>().velocity = new Vector2(-5f, 0f);
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update(){
+       
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D hit){
+
+        int rand = Random.Range(0, 4);
+        if (rand == 0)
+        {
+            this.GetComponent<Rigidbody2D>().velocity = new Vector2(2f, -5f);
+        }
+        else if (rand == 1)
+        {
+            this.GetComponent<Rigidbody2D>().velocity = new Vector2(3f, 5f);
+
+        }
+        else if (rand == 2)
+        {
+            this.GetComponent<Rigidbody2D>().velocity = new Vector2(5f, -4f);
+
+        }
+        else if (rand == 3)
+        {
+            this.GetComponent<Rigidbody2D>().velocity = new Vector2(-5f, -2f);
+
+        }
+  
+    }
+}
---
+++ Assets/scr.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Alien")
+        {
+            other.GetComponent<Canvas>().enabled = true;
+        }
+        else if (other.gameObject.tag == "Alien (1)")
+        {
+            other.GetComponent<Canvas>().enabled = true;
+        }
+        else if (other.gameObject.tag == "Alien (2)")
+        {
+            other.GetComponent<Canvas>().enabled = true;
+        }
+        else if (other.gameObject.tag == "Alien (3)")
+        {
+            other.GetComponent<Canvas>().enabled = true;
+        }
+        else if (other.gameObject.tag == "Alien (4)")
+        {
+            other.GetComponent<Canvas>().enabled = true;
+        }
+    }
+}
